6b492bbc2553a0057f9851ed99f72d740771fedd
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-09-28 04:25:30 -0700

    first commit

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -12,8 +12,6 @@ export default function Contact() {
                         <ul>
                             <li>Gmail : Muskanmadni153@gmail.com</li>
                             <li>Contact : 03408051451</li>
-                            <li><a href="https://github.com/Muskanmadni">GitHub : https://github.com/Muskanmadni</a></li>
-                            <li><a href="https://www.linkedin.com/in/umm-e-habiba-733838277/">linkdin : https://www.linkedin.com/in/umm-e-habiba-733838277/</a></li>
                         </ul>
                 </div>
             </div>
