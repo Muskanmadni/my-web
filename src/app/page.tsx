21f766ba4d21f6ea8939472a1085748c63e6231f
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-09-29 02:26:50 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { Footer } from "@/components/footer";
 import {Header} from "@/components/header";
-import { Home } from "@/components/header";
-import { Text } from "@/components/header";
 
 
 export default function home(){
   return(
     <div>
       <Header/>
-      <Home/>
-      <Text/>
       <Footer/>
     </div>
   )
